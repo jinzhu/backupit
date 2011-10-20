2a51a474329bff1a940ccd86f37a279622caa45f
frankyue <frankyue1019@gmail.com>
2011-10-20 12:41:59 +0800

    attribute init set fix

--- lib/backup/attribute.rb
+++ lib/backup/attribute.rb
@@ -4,7 +4,7 @@ module Backup
       attrs.flatten.each do |attr|
         class_eval <<-METHOD
           def #{attr}(value=nil)
-            instance_variable_set("@#{attr}", value) if value
+            instance_variable_set("@#{attr}", value) unless value.nil?
             instance_variable_get("@#{attr}")
           end
         METHOD
