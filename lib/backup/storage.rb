9c102f23924b69a179086b5bcfd7efaec66881f7
frankyue <frankyue1019@gmail.com>
2015-09-01 11:25:22 +0800

    postgresql dump with custome format

--- lib/backup/storage.rb
+++ lib/backup/storage.rb
@@ -110,7 +110,7 @@ module Backup
         postgresql_set_password = "PGPASSWORD=\"#{postgresql.password}\"" if postgresql.password
 
         tmpfile = Tempfile.new('postgresql.sql')
-        run_with_changes("ssh #{@ssh_host} '#{postgresql_set_password}; pg_dump #{postgresql_config} > #{tmpfile.path}'", postgresql_set_password) &&
+        run_with_changes("ssh #{@ssh_host} '#{postgresql_set_password}; pg_dump -F c #{postgresql_config} > #{tmpfile.path}'", postgresql_set_password) &&
         run_with_changes("scp #{@scp_host}:#{tmpfile.path} '#{backup_file}'") &&
         run_with_changes("ssh #{@ssh_host} 'rm #{tmpfile.path}'")
 
